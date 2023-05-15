79acf080946ad0cbd958916b597b0512a804d891
Yevheniia <yevheniialyzun@gmail.com>
2023-05-15 15:00:29 +0300

    change js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 fetch(
-  "http://api.openweathermap.org/data/2.5/weather?q=KYIV&units=metric&APPID=5d066958a60d315387d9492393935c19 "
+  "http://api.openweathermap.org/data/2.5/weather?q=KYIV&units=metric&APPID=5d066958a60d315387d9492393935c19"
 )
   .then((response) => response.json())
   .then((items) => {
