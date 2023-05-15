11c58029231f6d39fe745996b2cf045e0aa936bb
Yevheniia <yevheniialyzun@gmail.com>
2023-05-15 14:11:43 +0300

    add widget

---
+++ app.js
@@ -0,0 +1,33 @@
+let weatherOfCity = prompt("Your city");
+fetch(
+  `http://api.openweathermap.org/data/2.5/weather?q=${weatherOfCity.toUpperCase()}&units=metric&APPID=5d066958a60d315387d9492393935c19`
+)
+  .then((response) => response.json())
+  .then((items) => {
+    console.log(items);
+    let temp = document.querySelector(".temp");
+    temp.textContent = items.main.temp;
+
+    let pressure = document.querySelector(".pressure");
+    pressure.textContent = items.main.pressure;
+
+    let humidity = document.querySelector(".humidity");
+    humidity.textContent = items.main.humidity;
+
+    let speed = document.querySelector(".speed");
+    speed.textContent = items.wind.speed;
+    let deg = document.querySelector(".deg");
+
+    let descr = document.querySelector(".descr-box");
+    descr.textContent = items.weather[0].description.toUpperCase();
+
+    let city = document.querySelector(".city");
+    city.textContent = items.name.toUpperCase();
+
+    let icon = document.querySelector(".icon");
+
+    icon.setAttribute(
+      "src",
+      `http://openweathermap.org/img/w/${items.weather[0].icon}.png`
+    );
+  });
